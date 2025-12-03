49066c4e4ee291e6e334c37d5d25fe830970912a
somefan0102 <rntangiri@gmail.com>
2025-12-03 14:45:30 -0500

    added puzzle 2025-3 (py)

---
+++ 2025/3.py
@@ -0,0 +1,18 @@
+import sys
+
+B = len(sys.argv) > 1 and sys.argv[1] == "b"
+count = 12 if B else 2
+result = []
+
+for bank in sys.stdin.readlines():
+  bank = bank.strip()
+  joltage = ""
+
+  for _ in range(count):
+    safe = max(bank[:len(bank)-count+len(joltage)+1])
+    joltage += safe
+    bank = bank[bank.index(safe)+1:]
+
+  result.append(int(joltage))
+
+print(sum(result))
--- README.md
+++ README.md
@@ -8,6 +8,8 @@
 | ---       | ---       | ---
 | [2025-01] | [py](2025/1.py) [lua](2025/1.lua)
 | [2025-02] | [py](2025/2.py) | Slow, will reimplement in C
+| [2025-03] | [py](2025/3.py)
 
 [2025-01]: https://adventofcode.com/2025/day/1
 [2025-02]: https://adventofcode.com/2025/day/2
+[2025-03]: https://adventofcode.com/2025/day/3
