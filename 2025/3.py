8da78be0869c622380eef194984d6805d54192ee
somefan0102 <rntangiri@gmail.com>
2025-12-05 08:25:13 -0500

    modified puzzle 2025-3 (py) fixed spacing and favored counter over table

--- 2025/3.py
+++ 2025/3.py
@@ -2,17 +2,17 @@ import sys
 
 B = len(sys.argv) > 1 and sys.argv[1] == "b"
 count = 12 if B else 2
-result = []
+result = 0
 
 for bank in sys.stdin.readlines():
-  bank = bank.strip()
-  joltage = ""
+    bank = bank.strip()
+    joltage = ""
 
-  for _ in range(count):
-    safe = max(bank[:len(bank)-count+len(joltage)+1])
-    joltage += safe
-    bank = bank[bank.index(safe)+1:]
+    for _ in range(count):
+        safe = max(bank[:len(bank)-count+len(joltage)+1])
+        joltage += safe
+        bank = bank[bank.index(safe)+1:]
 
-  result.append(int(joltage))
+    result += int(joltage)
 
-print(sum(result))
+print(result)
