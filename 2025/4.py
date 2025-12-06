97e4175a68a1fcf3b907379449a0cb32fd13a012
somefan0102 <rntangiri@gmail.com>
2025-12-05 23:55:03 -0500

    modified puzzle 2025-4 (py)
    aligned continue statements

--- 2025/4.py
+++ 2025/4.py
@@ -10,16 +10,24 @@ while not done:
     prev = grid.copy()
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j] != '@': continue
+            if grid[i][j] != '@':
+                continue
+
             count = 0
+
             for n in NEIGHBORS:
                 n_i, n_j = i+n[0], j+n[1]
-                if n_i < 0 or  n_j < 0 or n_i > len(grid[i])-1 or n_j > len(grid[j])-1: continue
+
+                if n_i < 0 or  n_j < 0 or n_i > len(grid[i])-1 or n_j > len(grid[j])-1:
+                    continue
+
                 if grid[n_i][n_j] == '@':
                     count += 1
+
             if count < 4:
                 prev[i] = prev[i][:j] + "." + prev[i][j+1:]
                 total += 1
+
     done = grid == prev if B else True
     grid = prev.copy()
 
